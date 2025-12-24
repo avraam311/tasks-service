282208ceddcfc0ef0d53e59882bb866bbd4b176b
Ibragim <yandaroviba@mail.ru>
2025-12-24 15:49:51 +0300

    add logger

---
+++ internal/infra/logger/logger.go
@@ -0,0 +1,34 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+func Init(levelStr string, jsonFormat bool) {
+	var level slog.Level
+	switch levelStr {
+	case "debug":
+		level = slog.LevelDebug
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	opts := &slog.HandlerOptions{
+		Level:     level,
+		AddSource: true,
+	}
+
+	var handler slog.Handler
+	if jsonFormat {
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	} else {
+		handler = slog.NewTextHandler(os.Stdout, opts)
+	}
+
+	slog.SetDefault(slog.New(handler))
+}
