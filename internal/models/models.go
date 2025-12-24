e6210dfcb8d5204f713e30217b79b945e9831679
Ibragim <yandaroviba@mail.ru>
2025-12-24 19:00:14 +0300

    add task models

---
+++ internal/models/models.go
@@ -0,0 +1,14 @@
+package models
+
+type TaskDTO struct {
+	Header      string `json:"header" validate:"required"`
+	Description string `json:"description" validate:"required"`
+	Finished    bool   `json:"finished"`
+}
+
+type TaskDomain struct {
+	ID          uint   `json:"id" validate:"required"`
+	Header      string `json:"header" validate:"required"`
+	Description string `json:"description" validate:"required"`
+	Finished    bool   `json:"finished" validate:"required"`
+}
