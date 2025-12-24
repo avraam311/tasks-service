27f6c2b8b581d9d65d0cb16bb31831a8b3aa32a8
Ibragim <yandaroviba@mail.ru>
2025-12-24 19:06:05 +0300

    set up tasks handler

---
+++ internal/api/handlers/tasks/handler.go
@@ -0,0 +1,25 @@
+package tasks
+
+import (
+	"context"
+
+	"github.com/avraam311/tasks-service/internal/models"
+)
+
+type Service interface {
+	CreateTask(ctx context.Context, task *models.TaskDTO) (uint, error)
+	GetAllTasks(ctx context.Context) ([]*models.TaskDomain, error)
+	GetTask(ctx context.Context, taskID uint) (*models.TaskDomain, error)
+	UpdateTask(ctx context.Context, taskID uint, task *models.TaskDTO) error
+	DeleteTask(ctx context.Context, taskID uint) error
+}
+
+type Handler struct {
+	service Service
+}
+
+func New(service Service) *Handler {
+	return &Handler{
+		service: service,
+	}
+}
